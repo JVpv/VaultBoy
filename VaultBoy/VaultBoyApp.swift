aefd2d6b1f36ef1370c67ed1b755a54b5e32bb26
JVpv <jose.vpv7@gmail.com>
2025-07-10 13:58:17 -0400

    Initial Commit

---
+++ VaultBoy/VaultBoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaultBoyApp.swift
+//  VaultBoy
+//
+//  Created by Jose Vitor Prado on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct VaultBoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VaultBoyTests/VaultBoyTests.swift
@@ -0,0 +1,17 @@
+//
+//  VaultBoyTests.swift
+//  VaultBoyTests
+//
+//  Created by Jose Vitor Prado on 10/07/25.
+//
+
+import Testing
+@testable import VaultBoy
+
+struct VaultBoyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
